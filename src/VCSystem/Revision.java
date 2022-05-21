4c819d0e1ef8f71f60f3f905d00d5d6e1f395ef5
JDNorberto <jd.norberto@campus.fct.unl.pt>
2022-05-21 17:10:29 +0000

    New classes

---
+++ src/VCSystem/Revision.java
@@ -0,0 +1,13 @@
+package VCSystem;
+
+public interface Revision {
+
+	int getNum();
+
+	String getAuthor();
+
+	String getDate();
+
+	String getComment();
+
+}
---
+++ src/VCSystem/RevisionClass.java
@@ -0,0 +1,29 @@
+package VCSystem;
+
+public class RevisionClass implements Revision {
+
+	@Override
+	public int getNum() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getAuthor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getDate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getComment() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
